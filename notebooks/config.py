284d537b204820f51650e4b67393bf8e0e877f55
Alx-Lebeau <alexis.lebeau@protonmail.com>
2025-12-26 10:30:26 +0100

    Update config.py

--- notebooks/config.py
+++ notebooks/config.py
@@ -4,6 +4,8 @@ couleurs = {
     "nucleaire": "#e4a600",
     "hydraulique": "#2772b0",
     "gaz": "#f20808",
+    "solaire":"#d66b0e",
+    "eolien" :"#71cbb7",
     "echanges": "#969696",
     "imports":"#969696",
     "exports":"#969696"
